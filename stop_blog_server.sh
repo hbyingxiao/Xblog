e2e036695a4f73a23e0cc46089bb6dbffce51709
Aquarian-Age <xiaoyaoke7630@gmail.com>
2018-06-13 10:46:15 +0800

    停止脚本

---
+++ stop_blog_server.sh
@@ -0,0 +1,22 @@
+#=============================================================================
+#     FileName: start_blog_server.sh
+#         Desc:  停止程序
+#       Author: LiangZi
+#        Email: eGlhb3lhb2tlNzYzMEBzaW5hLmNvbQo=
+#     HomePage: https://aquarian-age.github.io/
+#      Version: 0.0.1
+#   LastChange: 2018-06-13 07:51:10
+#      History:
+#=============================================================================
+#!/bin/bash
+
+# set -v 
+
+kill $(ps aux | grep php | head -n 1 | cut -b '10-16')
+kill $(ps aux | grep php | head -n 1 | cut -b '10-16')
+
+ps aux | grep php
+
+# kill $(ps aux | grep php | awk 'NR==1{print}' | awk '{print $2}')
+# kill $(ps aux | grep php | awk 'NR==2{print}' | awk '{print $2}')
+
